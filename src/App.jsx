0a4911914a0b4b3f80a90d8f43db9bba020a4b10
Markku Vaara <markku.vaara45@gmail.com>
2024-08-24 14:24:42 +0300

    Added basic styles and rudimentary page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 
-const App = () => (
-  <div>
-    <p>Smoothie recipes</p>
-  </div>
-)
+import React, { useState, useEffect, useCallback } from 'react';
+
+const App = () => {
+
+  return (
+
+    <div className="mainapp">
+      <div className="title">
+        <h2>Smoothie recipes</h2>
+      </div>
+      <div className="banner">
+        <p>Frontpage Recipes Contact </p>
+      </div>
+      <div className="main">
+        <p>Recipes here</p>
+      </div>
+    </div>
+
+  )
+
+}
 
 export default App;
