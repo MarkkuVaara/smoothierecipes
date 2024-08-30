12f246c73561f8158c512a46b53f56416c9cb1da
Markku Vaara <markku.vaara45@gmail.com>
2024-08-30 12:10:40 +0300

    Refined page structure and added apple logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 
 import React, { useState, useEffect, useCallback } from 'react';
+import logo from './images/apple_logo.png';
 
 const App = () => {
 
   return (
 
     <div className="mainapp">
-      <div className="title">
-        <h2>Smoothie recipes</h2>
-      </div>
       <div className="banner">
-        <p>Frontpage Recipes Contact </p>
+        <img className="applelogo" src={logo} alt={logo} />
+        <p>Home</p>
+        <p>Recipes</p>
+        <p>About us</p>
+        <p>Blog</p>
       </div>
       <div className="main">
         <p>Recipes here</p>
