654a40f7d954eedb07b709cdfadb5bb1913e1a13
Markku Vaara <markku.vaara45@gmail.com>
2024-09-11 20:23:24 +0300

    Refined fonts and font sizes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="frontpage">
             <img className="smoothieimage" src={Frontpagesmoothie} alt={Frontpagesmoothie}/>
             <div className="fronttext">
-                <p>Discover delicious and nutritious smoothie ideas tailored to your taste. Whether you're craving something fruity, green, or indulgent, we've got the perfect blend for you.</p>
+                <h3>Welcome to the FRUTIY FUEL! Discover delicious and nutritious smoothie ideas tailored to your taste. Whether you're craving something fruity, green, or indulgent, we've got the perfect blend for you.</h3>
                 <h3>Let's get BLENDING!</h3>
             </div>
         </div>
