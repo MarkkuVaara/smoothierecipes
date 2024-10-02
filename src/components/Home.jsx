a28d0b90e82c829481de10a1db04bd1cbf4693f7
Markku Vaara <markku.vaara45@gmail.com>
2024-10-02 18:15:19 +0300

    More start window style refinement and addition of newsletter image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Frontpagesmoothie from '../images/frontpagesmoothie.jpg';
+import Newsletterimage from '../images/newsletterimage.jpg';
 
 const Home = () => {
 
@@ -9,6 +10,9 @@ const Home = () => {
             <div className="fronttext">
                 <h3>Welcome to the FRUITY FUEL! Discover delicious and nutritious smoothie ideas tailored to your taste. Whether you're craving something fruity, green, or indulgent, we've got the perfect blend for you.</h3>
                 <h2>Let's get BLENDING!</h2>
+                <div className="newsletter">
+                    <img className="letterimage" src={Newsletterimage} alt={Newsletterimage} />
+                </div>
             </div>
         </div>
     )
