ff6c6cddfabf72a17b8d2b854191fbabe245e1aa
Markku Vaara <markku.vaara45@gmail.com>
2024-10-04 21:16:13 +0300

    Refined newsletter popup styles

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import logo from '../images/apple_logo.png';
  
 const Newsletter = (props) => {
 
   return (
     <div className="popup-box">
       <div className="newsletter-box">
-        <div>
+        <img className="applelogo" src={logo} alt={logo}></img>
+        <div className="newsletter-texts">
+            <p>Subscribe, to keep in touch!</p>
+            <input default="Enter your e-mail here" />
+        </div>
+        <div className="newsletter-send-icon">
           <div onClick={props.handleClose}>Send</div>
         </div>   
         <div className="newsletter-close-icon">
