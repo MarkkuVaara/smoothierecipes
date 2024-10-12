fcbbe22a5136984f2913b6eb2e77d719535ab024
Markku Vaara <markku.vaara45@gmail.com>
2024-10-13 00:08:15 +0300

    Changed border colors according to the recipe subpage

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -15,12 +15,12 @@ const Recipes = ({ subpage }) => {
         <div className="recipiespage">
             <Slider dots={false} infinite={true} speed={500} slidesToShow={1}
                 slidesToScroll={1} autoplay={false} autoplaySpeed={3000} initialSlide={subpage} key={subpage}>
-            <div className="recipe">
+            <div className="recipe subpageban">
                 <img className="recipeimage" src={Banana} alt={Banana} title="Banana smoothie in a jar" />
                 <div className="recipetext">
                     <h3>BANANAAAAAA!</h3>
                     <div className="rectext">
-                        <strong><h4>Ingredients:</h4></strong>
+                        <strong><h4>INGREDIENTS:</h4></strong>
                         <h4>- 1 ripe banana</h4>
                         <h4>- 1 cup of milk (or almond milk for a dairy-free option)</h4>
                         <h4>- 1/2 cup yogurt (plain or vanilla)</h4>
@@ -28,7 +28,7 @@ const Recipes = ({ subpage }) => {
                         <h4>- A handful of ice cubes</h4>
                     </div>
                     <div className="rectext">
-                        <strong><h4>Instructions:</h4></strong>
+                        <strong><h4>INSTRUCTIONS:</h4></strong>
                         <h4>1. Peel the banana and place it to the blender.</h4>
                         <h4>2. Add the milk, yogurt, and honey or maple syrup if you want a bit of sweetness.</h4>
                         <h4>3. Toss in a handful of ice cubes.</h4>
@@ -37,12 +37,12 @@ const Recipes = ({ subpage }) => {
                     </div>
                 </div>
             </div>
-            <div className="recipe">
+            <div className="recipe subpageblue">
                 <img className="recipeimage" src={Blueberry} alt={Blueberry} title="Blueberry smoothie in a jar" />
                 <div className="recipetext">
                     <h3>Blueberriesss!!!</h3>
                     <div className="rectext">
-                        <strong><h4>Ingredients:</h4></strong>
+                        <strong><h4>INGREDIENTS:</h4></strong>
                         <h4>- 1 cup of fresh or frozen blueberries</h4>
                         <h4>- 1 cup of milk (or almond milk for a dairy-free option)</h4>
                         <h4>- 1/2 cup yogurt (plain or vanilla)</h4>
@@ -50,7 +50,7 @@ const Recipes = ({ subpage }) => {
                         <h4>- A handful of ice cubes</h4>
                     </div>
                     <div className="rectext">
-                        <strong><h4>Instructions:</h4></strong>
+                        <strong><h4>INSTRUCTIONS:</h4></strong>
                         <h4>1. Place the blueberries in a blender.</h4>
                         <h4>2. Add the milk, yogurt, and honey or maple syrup if you want a bit of sweetness.</h4>
                         <h4>3. Toss in a handful of ice cubes.</h4>
@@ -59,12 +59,12 @@ const Recipes = ({ subpage }) => {
                     </div>
                 </div>
             </div>
-            <div className="recipe">
+            <div className="recipe subpagestraw">
                 <img className="recipeimage" src={Strawberry} alt={Strawberry} title="Strawberry smoothie in a jar" />
                 <div className="recipetext">
                     <h3>Strawberryyy!!!</h3>
                     <div className="rectext">
-                        <strong><h4>Ingredients:</h4></strong>
+                        <strong><h4>INGREDIENTS:</h4></strong>
                         <h4>- 1 cup of fresh or frozen strawberries</h4>
                         <h4>- 1 cup of milk (or almond milk for a dairy-free option)</h4>
                         <h4>- 1/2 cup yogurt (plain or vanilla)</h4>
@@ -72,7 +72,7 @@ const Recipes = ({ subpage }) => {
                         <h4>- A handful of ice cubes</h4>
                     </div>
                     <div className="rectext">
-                        <strong><h4>Instructions:</h4></strong>
+                        <strong><h4>INSTRUCTIONS:</h4></strong>
                         <h4>1. Place the strawberries in the blender.</h4>
                         <h4>2. Add the milk, yogurt, and honey or maple syrup if you want a bit of sweetness.</h4>
                         <h4>3. Toss in a handful of ice cubes.</h4>
